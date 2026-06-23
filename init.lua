840245c30ce2ebb7ee38d0bb34f179320e849978
Ayadi <ayadi.chamseddine84@gmail.com>
2026-06-23 22:21:50 +0100

    lsp the old way

--- init.lua
+++ init.lua
@@ -2,33 +2,32 @@ require("LazySetup")
 require("telescope")
 ------------ KeyMaps
 require("keymaps")
-require('lspconfig') --.clangd.setup({})
-
+require('lspconfig').clangd.setup({})
+require('lspconfig').rust_analyzer.setup({})
 
 require('mini.snippets').setup({})
 require('mini.completion').setup({})
 
 
 
-vim.lsp.config('clangd',{
-	on_attach = on_attach,
-	cmd = { "clangd"},
-	filetypes = {"c", "cpp"},
-	capabilities = capabilities
-
-})
-
-
-vim.lsp.config('rust',{
-	on_attach = on_attach,
-	cmd={"rust-analyzer"},
-	filetype={"rs"},
-	capabilities = capabilities
-})
+--vim.lsp.config('clangd',{
+--	on_attach = on_attach,
+--	cmd = { "clangd"},
+--	filetypes = {"c", "cpp"},
+--	capabilities = capabilities
+--})
 
 
-vim.lsp.enable({"clangd"}) 
-vim.lsp.enable({"rust"})
+--vim.lsp.config('rust',{
+--	on_attach = on_attach,
+--	cmd={"rust-analyzer"},
+--	filetype={"rs"},
+--	capabilities = capabilities
+--})
+
+
+-- vim.lsp.enable({"clangd"}) 
+-- vim.lsp.enable({"rust"})
 
 
 vim.cmd("colorscheme gruvbox")
